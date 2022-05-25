f5ae942289c5a9de4a84590b35c9aaebbbe58e81
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-05-25 10:09:26 +0200

    Fake API Created

---
+++ README.md
@@ -0,0 +1,19 @@
+# FAKE API
+
+### To deploy
+`npm run start`
+
+### Endpoint
+`http://localhost:3006/reviews`
+
+### Response Structure
+```json
+{
+    reviews: [
+        {
+            name: string,
+            rate: number
+        }
+    ],
+    recall: boolean
+}```
---
+++ index.js
@@ -0,0 +1,28 @@
+const { faker } = require("@faker-js/faker")
+const express = require("express")
+
+const app = express()
+const PORT = 3006
+
+// GET -> /reviews
+app.get('/reviews', (req, res) => {
+    // Number of users with reviews (Max 5)
+    const users = Math.floor(Math.random() * 5) + 1
+    // Random recall value
+    const recall = faker.datatype.boolean()
+
+    const reviews = []
+    for (let i = 0; i < users; i++) {
+        const newUser = {
+            name: faker.name.findName(),
+            rate: faker.datatype.number({ max: 5 })
+        }
+        reviews.push(newUser)
+    }
+    
+    return res.json({reviews, recall})
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})
